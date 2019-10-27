9364a89d11a98e0cf7a7e2b01003e192fa12867f
Brian Lee <brianalee@gmail.com>
2019-10-27 15:19:08 -0400

    Update entrypoint.sh
    more debug

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -3,9 +3,11 @@ echo 'hello world'
 pwd
 ls -alrt
 echo '👍 ENTRYPOINT HAS STARTED—INSTALLING THE GEM BUNDLE'
-bundle install > /dev/null 2>&1
+bundle install
 bundle list | grep "jekyll ("
 echo '👍 BUNDLE INSTALLED—BUILDING THE SITE'
 bundle exec jekyll build
 echo '👍 THE SITE IS BUILT—GREAT SUCCESS'
+pwd
+ls -alrt
 
